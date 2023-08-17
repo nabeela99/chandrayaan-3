b9e484b2461a0e18bd7bcbab223eab5a0340098f
Nabeela <nabeela.jahan99@gmail.com>
2023-08-17 20:23:40 +0530

    Test case 3

--- src/test/java/com/nabeela/tdd/SpacecraftControlTest.java
+++ src/test/java/com/nabeela/tdd/SpacecraftControlTest.java
@@ -17,5 +17,8 @@ public class SpacecraftControlTest {
 
         SpacecraftStatus expected2 = new SpacecraftStatus(new int[]{0,-1,0}, "N");
         Assert.assertEquals(expected2, spacecraftControl.controlSpacecraft(List.of("b"),"N"));
+
+        SpacecraftStatus expected3 = new SpacecraftStatus(new int[]{0,0,0}, "E");
+        Assert.assertEquals(expected3, spacecraftControl.controlSpacecraft(List.of("r"),"N"));
     }
 }
